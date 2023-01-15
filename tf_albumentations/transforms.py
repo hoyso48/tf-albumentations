b749a413dab51807a78cacc212975c7f0b4dc7c7
hoyso48 <81813194+hoyso48@users.noreply.github.com>
2023-01-15 18:17:46 +0900

    Update transforms.py

--- tf_albumentations/transforms.py
+++ tf_albumentations/transforms.py
@@ -651,6 +651,24 @@ def ColorJitter(p=0.5, hue=(-0.05,0.05), brightness=(0.8,1.2), saturation=(0.7,1
                  Contrast(p=1, factor=contrast),
                  ], p=p, n=4, with_replacement=False)
 
-# def ShiftScaleRotate(p=0.5, shift=(-0.0625, 0.0625), scale=(0.9,1.1), rotate=(-0.45,0.45), replace=0):
-#   if isinstance(scale, tuple):
-    
+def RandomAffine(p=0.5, rotate=(-45,45), shear=(-0.15,0.15), translate=(-0.05,0.05), replace=0):
+    return A.Choice([A.Rotate(p=1,degrees=rotate,replace=replace), 
+                     A.Shear(p=1,level=shear,replace=replace), 
+                     A.Translate(p=1,level=translate,replace=replace)], p=p, with_replacement=False, n=3)
+  
+def JitterV2(p=0.5, level=1, color=True, affine=True, replace=0):
+    #inspired from simclr-v2 augmentation
+    policy = []
+    if color:
+      policy.append(ColorJitter(p=p, 
+                                hue=(-0.2*level, 0.2*level), 
+                                brightness=(1-0.8*level,1+0.8*level), 
+                                saturation=(1-0.8*level,1+0.8*level), 
+                                contrast=(1-0.8*level,1+0.8*level)))
+    if affine:
+      policy.append(RandomAffine(p=p,
+                                 rotate=(-40*level,40*level),
+                                 shear=(-0.4*level,0.4*level),
+                                 translate=(-0.2*level,0.2*level), replace=replace))
+    return A.Sequence(policy, p=1)
+  
